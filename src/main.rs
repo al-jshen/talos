1651d2de856f4f466a76de22073f47a667151027
Jeff Shen <jshen2014@hotmail.com>
2021-08-06 00:43:45 -0400

    pas integer data

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ use talos::samplers::{Gibbs, Sampler};
 use talos::*;
 use talos_procs::model;
 
+pub enum Data {
+    Float(f64),
+    Int(i32),
+    FloatArray(Vector),
+    IntArray(Vec<i32>),
+}
+
 fn main() {
     const n: usize = 2;
 
@@ -18,8 +25,8 @@ fn main() {
     x = x + Normal::new(0., 1.).sample_n(n);
     let mut y = (&x) * 5. + 2.;
     y = y + Normal::new(0., 1.).sample_n(n);
-    data.insert("xobs", x);
-    data.insert("yobs", y);
+    data.insert("xobs", Data::FloatArray(x));
+    data.insert("yobs", Data::FloatArray(y));
 
     for chain in s.sample_par(lnlik, &params, &data, 10000, 4) {
         for samp in chain {
@@ -29,17 +36,25 @@ fn main() {
 }
 
 #[model("f64")]
-fn lnlik(params: &[f64], data: &HashMap<&str, Vector>) {
+fn lnlik(params: &[f64], data: &HashMap<&str, Data>) {
     let (m, b) = (params[0], params[1]);
     let x = &params[2..4];
     let y = &params[4..6];
+    let xobs = match &data["xobs"] {
+        Data::FloatArray(arr) => arr,
+        _ => panic!(),
+    };
+    let yobs = match &data["xobs"] {
+        Data::FloatArray(arr) => arr,
+        _ => panic!(),
+    };
 
     normal!(m; 4_f64, 1_f64);
     laplace!(b; 2_f64, 1_f64);
 
     for i in 0..2 {
-        normal!(data["xobs"][i]; x[i], 1.);
-        normal!(data["yobs"][i]; x[i], 1.);
+        normal!(xobs[i]; x[i], 1.);
+        normal!(yobs[i]; x[i], 1.);
         normal!(y[i] ; x[i] * m + b, 1.);
     }
 }
