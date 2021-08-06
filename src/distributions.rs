f20f06a3000d144d89ca382b7b35a304375e1085
Jeff Shen <jshen2014@hotmail.com>
2021-08-06 01:47:01 -0400

    implement poisson, cauchy, lognormal, rayleigh, pareto distributions

--- src/distributions.rs
+++ src/distributions.rs
@@ -21,7 +21,7 @@ macro_rules! uniform {
     ( $var: expr; $lower: expr, $upper: expr ) => {{
         assert!($upper > $lower, "Upper must be greater than lower.");
         if $lower <= $var && $var <= $upper {
-            (1._f64 / ($upper - $lower)).ln()
+            -($upper - $lower).ln()
         } else {
             f64::NEG_INFINITY
         }
@@ -44,7 +44,7 @@ macro_rules! gamma {
 macro_rules! laplace {
     ( $var: expr; $mu: expr, $b: expr ) => {{
         assert!($b > 0., "Scale parameter `b` must be positive.");
-        (1_f64 / (2_f64 * $b)).ln() - ($var - $mu).abs() / $b
+        -$b.ln() - ($var - $mu).abs() / $b
     }};
 }
 
@@ -92,5 +92,52 @@ macro_rules! binomial {
         //     - functions::gamma(success + 1.).ln()
         //     - functions::gamma(trials - success + 1.).ln() +
         success * $theta.ln() + (trials - success) * (1. - $theta).ln()
+    }};
+}
+
+#[macro_export]
+macro_rules! poisson {
+    ( $n: expr; $lambda: expr ) => {
+        assert!($n >= 0, "n must be a non-negative integer.");
+        $n as f64 * $lambda.ln() - $lambda
+    };
+}
+
+#[macro_export]
+macro_rules! cauchy {
+    ( $y: expr; $mu: expr, $sigma: expr ) => {{
+        assert!($sigma > 0., "Sigma must be positive.");
+        ($sigma / ($sigma.powi(2) + ($mu - $y).powi(2))).ln()
+    }};
+}
+
+#[macro_export]
+macro_rules! lognormal {
+    ( $y: expr; $mu: expr, $sigma: expr ) => {{
+        assert!($sigma > 0., "Sigma must be positive.");
+        assert!($y > 0., "Varaible must be positive.");
+        -0.5 * (($y.ln() - $mu) / $sigma).powi(2) - $y.ln() - $sigma.ln()
+    }};
+}
+
+#[macro_export]
+macro_rules! rayleigh {
+    ( $y: expr; $sigma: expr) => {{
+        assert!($sigma > 0., "Sigma must be positive.");
+        assert!($y >= 0., "Variable must be non-negative.");
+        -$y.powi(2) / (2. * $sigma.powi(2)) + y.ln() - 2. * $sigma.ln()
+    }};
+}
+
+#[macro_export]
+macro_rules! pareto {
+    ( $y: expr; $ymin: expr, $alpha: expr) => {{
+        assert!($ymin > 0., "y_min must be positive.");
+        assert!($alpha > 0., "alpha must be positive.");
+        assert!(
+            $y >= $ymin,
+            "Variable y must be at least as large as y_min."
+        );
+        $alpha.ln() + $alpha * $y_min.ln() - ($alpha - 1.) * $y.ln()
     }};
 }
