d7c3990cc3b7c94bc91f32fce062eb6f0e82e169
Jeff Shen <jshen2014@hotmail.com>
2021-08-06 00:26:07 -0400

    implement parallel sampler method; move update_dim inside gibbs

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,39 @@ use talos::*;
 use talos_procs::model;
 
 fn main() {
-    let s = Gibbs::new(&[0.1, 0.1]);
+    const n: usize = 2;
 
-    let params = [0., 0.];
+    let s = Gibbs::new(&[0.05; 2 + 2 * n]);
+
+    let params = [0.; 2 + 2 * n];
 
     let mut data = HashMap::new();
-    let x = linspace(0., 10., 1000);
+    let mut x = linspace(0., 10., n);
+    x = x + Normal::new(0., 1.).sample_n(n);
     let mut y = (&x) * 5. + 2.;
-    y = y + Normal::new(0., 1.).sample_n(x.len());
-    data.insert("x", x);
-    data.insert("y", y);
+    y = y + Normal::new(0., 1.).sample_n(n);
+    data.insert("xobs", x);
+    data.insert("yobs", y);
 
-    for samp in s.sample(lnlik, &params, &data, 10000) {
-        println!("{},{}", samp[0], samp[1]);
+    for chain in s.sample_par(lnlik, &params, &data, 10000, 4) {
+        for samp in chain {
+            println!("{},{}", samp[0], samp[1]);
+        }
     }
 }
 
 #[model("f64")]
 fn lnlik(params: &[f64], data: &HashMap<&str, Vector>) {
     let (m, b) = (params[0], params[1]);
+    let x = &params[2..4];
+    let y = &params[4..6];
 
     normal!(m; 4_f64, 1_f64);
     laplace!(b; 2_f64, 1_f64);
 
-    for (&xi, &yi) in data["x"].iter().zip(&data["y"]) {
-        normal!(yi ; xi * m + b, 1.);
+    for i in 0..2 {
+        normal!(data["xobs"][i]; x[i], 1.);
+        normal!(data["yobs"][i]; x[i], 1.);
+        normal!(y[i] ; x[i] * m + b, 1.);
     }
 }
--- src/samplers.rs
+++ src/samplers.rs
@@ -1,17 +1,13 @@
-use reverse::*;
-
 use compute::prelude::{Distribution, Normal};
+use rayon::prelude::*;
 
 pub struct Gibbs {
     stepsizes: Vec<f64>,
 }
 
 pub trait Sampler {
-    type V;
-    fn update_dim<'a, F, S>(&self, f: F, params: &[Self::V], data: &S, i: usize) -> Self::V
-    where
-        F: Fn(&[Self::V], &S) -> Self::V;
-    fn update<'a, F, S>(&self, f: F, current_params: &[Self::V], data: &S) -> Vec<Self::V>
+    type V: Send + Sync + Copy;
+    fn step<'a, F, S>(&self, f: F, params: &[Self::V], data: &S) -> Vec<Self::V>
     where
         F: Fn(&[Self::V], &S) -> Self::V;
     fn sample<'a, F, S>(
@@ -23,6 +19,24 @@ pub trait Sampler {
     ) -> Vec<Vec<Self::V>>
     where
         F: Fn(&[Self::V], &S) -> Self::V;
+    fn sample_par<'a, F, S>(
+        &self,
+        f: F,
+        inits: &[Self::V],
+        data: &S,
+        n_samples: usize,
+        n_chains: usize,
+    ) -> Vec<Vec<Vec<Self::V>>>
+    where
+        F: Fn(&[Self::V], &S) -> Self::V + Copy + Send + Sync,
+        Self: Send + Sync,
+        S: Send + Sync,
+    {
+        (0..n_chains)
+            .into_par_iter()
+            .map(|_| self.sample(f, inits, data, n_samples))
+            .collect()
+    }
 }
 
 impl Gibbs {
@@ -40,28 +54,9 @@ impl Gibbs {
 
 impl Sampler for Gibbs {
     type V = f64;
-    /// Make a proposal for the ith dimensional parameter.
-    fn update_dim<'a, F, S>(&self, f: F, current_params: &[Self::V], data: &S, i: usize) -> Self::V
-    where
-        F: Fn(&[Self::V], &S) -> Self::V,
-    {
-        let mut proposed_params = current_params.to_vec();
-        proposed_params[i] = proposed_params[i] + Normal::new(0., self.stepsizes[i]).sample();
-
-        let current_likelihood = f(&current_params, data);
-        let proposed_likelihood = f(&proposed_params, data);
-
-        let p_accept = f64::min((proposed_likelihood - current_likelihood).exp(), 1.);
-
-        if alea::f64() < p_accept {
-            proposed_params[i]
-        } else {
-            current_params[i]
-        }
-    }
 
     /// Make a single proposal for the all parameters.
-    fn update<'a, F, S>(&self, f: F, current_params: &[Self::V], data: &S) -> Vec<Self::V>
+    fn step<'a, F, S>(&self, f: F, current_params: &[Self::V], data: &S) -> Vec<Self::V>
     where
         F: Fn(&[Self::V], &S) -> Self::V,
     {
@@ -73,7 +68,22 @@ impl Sampler for Gibbs {
         let mut running_params = current_params.to_vec();
 
         for i in 0..self.dims() {
-            running_params[i] = self.update_dim(&f, &running_params, data, i);
+            running_params[i] = {
+                let mut proposed_params = running_params.clone();
+                proposed_params[i] =
+                    proposed_params[i] + Normal::new(0., self.stepsizes[i]).sample();
+
+                let current_likelihood = f(&running_params, data);
+                let proposed_likelihood = f(&proposed_params, data);
+
+                let p_accept = f64::min((proposed_likelihood - current_likelihood).exp(), 1.);
+
+                if alea::f64() < p_accept {
+                    proposed_params[i]
+                } else {
+                    running_params[i]
+                }
+            };
         }
 
         running_params
@@ -97,7 +107,7 @@ impl Sampler for Gibbs {
         let mut running_params = inits.to_vec();
 
         for _ in 0..n_samples {
-            running_params = self.update(&f, &running_params, data);
+            running_params = self.step(&f, &running_params, data);
             samples.push(running_params.clone());
         }
 
